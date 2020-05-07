f0100e1bfdf364b766a43c2baea8405c534a8acc
AX99 <rapha.mensah@gmail.com>
2020-05-07 02:18:09 +0100

    Error catching & General cleanup

--- script.py
+++ script.py
@@ -1,17 +1,36 @@
 import json
+from dateutil.parser import parse
 from datetime import datetime as date
 
-data = json.load(open("ghananames.json"))
+#Load the json file with the days and corresponding names
+data = json.load(open("akannames.json"))
 
-#print(data["Monday"]["F"]) FOR Adjoa
+
 gender = input("Are you Male or Female? (M/F): ")
-y = int(input("Enter your year of birth: "))
-m = int(input("Enter your month of birth (1-12): "))
-d = int(input("Enter your day of birth: "))
+dob = input("Enter your date of birth in this format: 01-Jan-1980: ")
 
-day_name = date(y, m, d).strftime("%A")
-ghana_name = data[day_name][gender.upper()]
-print()
-print("You were born on a " + day_name)
-print("Your Ghanaian name is " + ghana_name)
-print()
+
+#User input checks 
+
+if gender.upper().str not in ("M","F"):
+    raise Exception("\n\nYou didn't enter the correct gender. \n\n")
+    
+try:
+    dayName = parse(dob).strftime("%A")
+    akanName = data[dayName][gender.upper()]    
+except:
+    print('\n\nYou didn\'t enter your date of birth correctly\n\n')
+    
+    
+
+#Print Values - exception if variables not set
+try:
+    print(
+        """
+        You were born on a %s
+        Your Ghanaian name is %s
+        """
+        % (dayName, akanName))
+except :
+    pass
+
