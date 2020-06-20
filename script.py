120f9d452d7864405c8a67faa0ab96bafbf02f0c
AX99 <rapha.mensah@gmail.com>
2020-06-20 06:45:38 +0100

    virtualenv

--- script.py
+++ script.py
@@ -5,22 +5,18 @@ from datetime import datetime as date
 #Load the json file with the days and corresponding names
 data = json.load(open("akannames.json"))
 
-
 gender = input("\nAre you Male or Female? (M/F): ")
-dob = input("\nEnter your date of birth in this format: 01-Jan-1980: ")
-
-
-#User input checks 
 
 if gender.upper().strip() not in ("M","F"):
     raise Exception("\n\nYou didn't enter the correct gender. \n\n")
+
+dob = input("\nEnter your date of birth in this format: 01-Jan-1980: ")
     
 try:
     dayName = parse(dob).strftime("%A")
     akanName = data[dayName][gender.upper()]    
 except:
     print('\n\nYou didn\'t enter your date of birth correctly\n\n')
-    
     
 
 #Print Values - exception if variables not set
